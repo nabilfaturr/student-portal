b9d29cb3af16f32d16f195d259c4e204093e4adb
nabilfaturr <nabilfaturr44@gmail.com>
2023-11-15 22:57:18 +0700

    setup mysql

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 import express from "express";
+import { db } from "./configs/db.js";
 
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  const sql = `SELECT * FROM mhs`;
+  db.query(sql, (error, result) => {
+    console.log(result);
+  });
+});
+
 app.listen(PORT, () => {
-    console.log(`Currently running on port ${PORT}... `)
+  console.log(`Currently running on port ${PORT}... `);
 });
